fb6c59359d26bd2d6d0affb9d5bed7a7886e0a81
EricMiddelhove <eric.middelhove@icloud.com>
2022-01-18 21:35:10 +0000

    fixed errors

--- pi-server/app.py
+++ pi-server/app.py
@@ -1,21 +1,23 @@
 from flask import Flask
 
 import subprocess
+import os
 
 app = Flask(__name__)
 
 
-@app.route("/startpc")
+@app.route("/start")
 def start_pc():
 	print("Waking PC")
-	subprocess.call("wakeonlan 2C:F0:5D:05:C0:A9")
+	os.popen("wakeonlan 2C:F0:5D:05:C0:A9")
+	return "OK"
 
 
 @app.route("/hibernate")
 def hibernate_pc():
 	print("Hibernate")
-	subprocess.call("curl http://192.168.2.11:5000/hibernate")
-
+	os.popen("curl -sS http://192.168.2.11:5000/hibernate")
+	return "OK"
 
 
 if __name__ == "__main__":
